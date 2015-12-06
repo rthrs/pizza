afa4d4ee2c598ecb69ce8bb2330a6aba7ba46ae6
Artur Myszkowski <am347189@students.mimuw.edu.pl>
2015-12-06 14:17:25 +0100

    pizza.hh added

---
+++ pizza.hh
@@ -0,0 +1,26 @@
+#ifndef __PIZZA_HH__
+#define __PIZZA_HH__
+
+template<typename... Kinds> struct Pizzeria {
+
+   template<typename Kind> struct make_pizza {
+      using ......... = type;
+   };
+/** To w strukturze reprezentujacej pizze **/
+   template<typename Kind> static constexpr size_t count() {
+   
+   };
+
+   static constexpr std::array<size_t, [N]> as_array() {
+
+   };
+   
+   using ........... = sliced_type;
+/*******************************************/
+};
+
+template<typename Pizza1, typename Pizza2> struct best_mix {
+
+};
+
+#endif /* __PIZZA_HH__ */
