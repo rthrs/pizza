7b9b8c86106bf0eaa5a2806e355fa2f9b4a10c1f
Artur Myszkowski <am347189@students.mimuw.edu.pl>
2015-12-08 19:40:03 +0100

    make_piiza + count

--- pizza.hh
+++ pizza.hh
@@ -1,22 +1,39 @@
 #ifndef __PIZZA_HH__
 #define __PIZZA_HH__
 
+#include <type_traits>
+
+
 template<typename... Kinds> struct Pizzeria {
 
-   template<typename Kind> struct make_pizza {
-      using type = /*smth*/;
-   };
-/** To w strukturze reprezentujacej pizze **/
+   template<size_t... Slices> struct Pizza {
+      using pizzeria = Pizzeria<Kinds ...>;
+
+
    template<typename Kind> static constexpr size_t count() {
-   
+      return ((std::is_same(Kind, Kinds)::value * Slices) + ...);
+   }
+
+
+ //  static constexpr std::array<size_t, [N]> as_array() {
+      
    };
 
-   static constexpr std::array<size_t, [N]> as_array() {
+   /** 
+    * nalezy znalezc ktory z kolei to jest typ 'Kind' liscie typow pizy w 
+    * pizzeri i dac mu w tym miejscu dla struktury Pizza 8 kawałków 
+    */ 
+   template<typename Kind> struct make_pizza {
+      using type = Pizza<(std::is_same(Kind, Kinds)::value ? default_slices_count : 0>
+   };
+
+   
+
 
    };
    
    using sliced_type = /*smth*/;
-/*******************************************/
+
 };
 
 template<typename Pizza1, typename Pizza2> struct best_mix {
